05a7965b0dc0979d04f365d0fb77b3c70dad837b
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-07-24 22:02:45 -0400

    end 18.1.5 pizza model added with mongoose

---
+++ models/Pizza.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require('mongoose');
+
+const PizzaSchema = new Scehma({
+    pizzaName: {
+        type: String
+    },
+    createdBy: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    size: {
+        type: String,
+        default: "Large"
+    },
+    toppings: [] // can also just list "Array" here as the datatype instead of the brackets
+});
+
+//create the Pizza model using the PizzaSchema
+const Pizza = model('Pizza', PizzaSchema);
+
+//export the Pizza model
+module.exports = Pizza;
