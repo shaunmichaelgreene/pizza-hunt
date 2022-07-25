fcd16eddec215560e0926995353e511d05325f9c
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-07-24 22:48:36 -0400

    api routes added and tied in

---
+++ routes/api/pizza-routes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const {
+    getAllPizza,
+    getPizzaById,
+    createPizza,
+    updatePizza,
+    deletePizza
+} = require('../../controllers/pizza-controller');
+
+// /api/pizzas
+
+router
+    .route('/')
+    .get(getAllPizza)
+    .post(createPizza);
+
+//set up GET one, PUT, and DELETE at /api/pizzas/:id
+router
+    .route('/:id')
+    .get(getPizzaById)
+    .post(updatePizza)
+    .delete(deletePizza);
+
+module.exports = router;
