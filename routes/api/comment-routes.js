ec5a50f3acb0f7f6f69a3d116ebe0ae870ab3110
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-07-26 20:33:17 -0400

    end 18.3.2, replies branch added

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const { addComment, removeComment } = require('../../controllers/comment-controller');
 
-// /api/comments/pizzaId
+// /api/comments/<pizzaId>
 router.route('/:pizzaId').post(addComment);
 
-// /api/comments/pizzaId/commentId
+// /api/comments/<pizzaId>/<commentId>
 router.route('/:pizzaId/:commentId').delete(removeComment);
 
 module.exports = router;
